a7a107f3a9686038c32c1b1e792bb0981bc443b3
Steeven <steeveric5@gmail.com>
2026-07-07 19:10:05 +0300

    feat(utilitaire): Add function invokeMethod and render

--- src/mg/itu/framework/utils/Utilitaire.java
+++ src/mg/itu/framework/utils/Utilitaire.java
@@ -5,6 +5,7 @@ import mg.itu.framework.annotation.UrlMapping;
 import mg.itu.framework.dto.MethodDTO;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Method;
 import java.net.URL;
@@ -16,14 +17,43 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import mg.itu.framework.dto.RequestMapping;
 import mg.itu.framework.exception.RouteDejaDefinieException;
+import mg.itu.framework.vue.ModelAndView;
+import mg.itu.framework.vue.ViewResolver;
 
 public class Utilitaire {
 
     public Utilitaire() {
     }
-    
+
+    public static Object invokeMethod(MethodDTO dto) throws Exception {
+        Object instance = dto.getMethod().getDeclaringClass()
+                .getDeclaredConstructor().newInstance();
+        return dto.getMethod().invoke(instance);
+    }
+
+    public static void render(Object result, ViewResolver vr,
+            HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        if (result instanceof String) {
+            req.getRequestDispatcher(vr.resolve((String) result))
+                    .forward(req, res);
+        } else if (result instanceof ModelAndView) {
+            ModelAndView mav = (ModelAndView) result;
+            if (mav.getData() != null) {
+                mav.getData().forEach(req::setAttribute);
+            }
+            req.getRequestDispatcher(vr.resolve(mav.getView()))
+                    .forward(req, res);
+        } else {
+            throw new IllegalArgumentException(
+                    "La methode doit retourner un String ou un ModelAndView");
+        }
+    }
 
     public Map<RequestMapping, MethodDTO> findMethod(List<Class<?>> classes) {
         Map<RequestMapping, MethodDTO> methodes = new LinkedHashMap<>();
